7c1530adbdc61ce01170f60fdf6357b72c568d15
alirio <mie21001@byui.edu>
2023-03-02 17:24:35 -0400

    add try/catch to endpoints

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,44 +18,70 @@ const findAll = async (_, res = response) => {
   // #swagger.tags = ['Contacts']
   // #swagger.description = 'Endpoint get all contacts'
 
-  const users = await Contact.find();
-  res.status(201).json({
-    users
-  });
+  try {
+    const users = await Contact.find();
+    res.status(201).json({
+      users
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: error
+    });
+  }
 };
 
 const findOne = async (req = request, res = response) => {
   // #swagger.tags = ['Contacts']
   // #swagger.description = 'Endpoint get a contact'
 
-  const user = await Contact.findById(req.params.id);
-  res.json({
-    user
-  });
+  try {
+    const user = await Contact.findById(req.params.id);
+    res.json({
+      user
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: error
+    });
+  }
 };
 
 const deleteContact = async (req = request, res = response) => {
   // #swagger.tags = ['Contacts']
   // #swagger.description = 'Endpoint delete a contact'
 
-  const user = await Contact.findByIdAndDelete(req.params.id);
-  res.status(200).json({
-    user
-  });
+  try {
+    const user = await Contact.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      user
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: error
+    });
+  }
 };
 
 const updateContact = async (req = request, res = response) => {
   // #swagger.tags = ['Contacts']
   // #swagger.description = 'Endpoint update a contact'
 
-  const { id } = req.params;
-  const data = req.body;
-
-  const user = await Contact.findByIdAndUpdate(id, data);
-
-  res.status(204).json({
-    user
-  });
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const user = await Contact.findByIdAndUpdate(id, data);
+    res.status(204).json({
+      user
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: error
+    });
+  }
 };
 
 module.exports = {
